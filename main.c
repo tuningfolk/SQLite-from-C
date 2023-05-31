55a488ba0ea484f8f3418ad14165fc558fdf0fd9
tuningfolk <alisahadges@gmail.com>
2023-05-31 11:08:21 +0530

    Added table struct

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include<stdint.h>
 #include<stdbool.h>
 #include<stddef.h>
 #include<stdlib.h>
 #include<string.h>
+
 typedef struct InputBuffer{
     char* buffer;
     size_t buffer_length;
@@ -24,8 +26,16 @@ typedef enum PrepareResult{
     PREPARE_UNRECOGNIZED_STATEMENT
 }PrepareResult;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 32
+typedef struct Row{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+}Row;
 typedef struct Statement{
     StatementType type;
+    Row row_to_insert; //only used by insert statement
 }Statement;
 
 InputBuffer* new_input_buffer(){
@@ -77,20 +87,81 @@ PrepareResult prepare_statement(InputBuffer* input, Statement* statement){
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-void execute_statement(Statement* statement){
+ExecuteResult execute_statement(Statement* statement){
     switch (statement->type)
     {
     case  STATEMENT_INSERT:
-        printf("This is where you would do an insert.\n");
-        break;
+        // printf("This is where you would do an insert.\n");
+        // break;
+        return execute_insert();
     case STATEMENT_SELECT:
-        printf("This is where you do a select.\n");
-        break;
+        // printf("This is where you do a select.\n");
+        // break;
+        return execute_select();
     default:
         break;
     }
 }
 
+#define size_of_attribute(Struct, Attribute) sizeof( ((Struct*)0)->Attribute )
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0; //I know why you might need this, but I can't tell how you would use this
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = EMAIL_OFFSET + EMAIL_SIZE; //or ID_SIZE + USERNAME_SIZE + EMAIL_SIZE
+
+/* 4 kilobytes because it's the same as a page used in the virtual systems of most computer architectures,
+this means one page in our database corresponds to one page in the operating system, the os will move pages in and out
+of memory as whole units instead of breaking them up*/
+/* 100 pages = arbitrary*/
+
+const uint32_t PAGE_SIZE = 4096;
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+typedef struct Table{
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+}Table;
+
+Table* create_table(){
+    Table* table= malloc(sizeof(Table));
+    table->num_rows = 0;
+    for(int i=0; i<TABLE_MAX_PAGES; i++){
+        table->pages[i] = NULL;
+    }
+    return table;
+}
+
+void free_table(Table* table){
+    for(int i=0; table->pages[i]; i++){ //an interesting way to put the limiting case
+        free(table->pages[i]);
+    }
+    free(table);
+}
+
+
+typedef enum ExecuteResult{
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+}ExecuteResult;
+
+void* row_slot(Table* table, uint32_t row_num){
+    uint32_t page_num = row_num/ROWS_PER_PAGE;
+    uint32_t row_offset = row_num%ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if(page == NULL){
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t byte_offset = page + row_offset*ROW_SIZE;
+    return page+byte_offset;
+}
+
+void
 
 
 int main(int argc, char* argv[]){

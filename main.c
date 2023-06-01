515faf8e34a42418eca28fb2acc7b8b8053568fc
tuningfolk <alisahadges@gmail.com>
2023-06-01 21:50:36 +0530

    Added serializing and deserializing

--- main.c
+++ main.c
@@ -108,7 +108,7 @@ ExecuteResult execute_statement(Statement* statement){
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0; //I know why you might need this, but I can't tell how you would use this
+const uint32_t ID_OFFSET = 0; //I know why you might need this, but I can't tell how you would use this, okay now I know, you need it for serializing
 const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = EMAIL_OFFSET + EMAIL_SIZE; //or ID_SIZE + USERNAME_SIZE + EMAIL_SIZE
@@ -142,6 +142,18 @@ void free_table(Table* table){
         free(table->pages[i]);
     }
     free(table);
+}
+
+void serialize_row(Row* source, void* destination){
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(Row* destination, void* source){
+    memcpy(&(destination->id), source + ID_OFFSET , ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 

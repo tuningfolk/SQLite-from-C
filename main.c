cfcb67a4e6e26510ac7408d9f6f19aa773c6ac7d
tuningfolk <alisahadges@gmail.com>
2023-07-27 15:38:43 +0530

    Made all fixes.

--- main.c
+++ main.c
@@ -29,7 +29,9 @@ typedef enum{
 typedef enum{
     PREPARE_SUCCESS,
     PREPARE_UNRECOGNIZED_STATEMENT,
-    PREPARE_SYNTAX_ERROR
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_STRING_TOO_LONG,
+    PREPARE_NEGATIVE_ID
 } PrepareResult;
 
 //just two values for now
@@ -141,18 +143,47 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer,Table* table){
     }
 }
 
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
+    statement->type = STATEMENT_INSERT;
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " ");
+    char* email = strtok(NULL, " ");
+
+    if(id_string==NULL || username==NULL || email==NULL){
+        return PREPARE_SYNTAX_ERROR;
+    }
+    int id = atoi(id_string);
+    //how to make sure id_string is a number?
+    if(id<0){
+        return PREPARE_NEGATIVE_ID;
+    }
+    if(strlen(username)>COLUMN_USERNAME_SIZE) return PREPARE_STRING_TOO_LONG;
+    if(strlen(email)>COLUMN_EMAIL_SIZE) return PREPARE_STRING_TOO_LONG;
+    
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username,username);
+    strcpy(statement->row_to_insert.email,email);
+    return PREPARE_SUCCESS;
+}
+
 //Our "SQL compiler". Now our compiler only understands two words
 PrepareResult prepare_statement(InputBuffer* input_buffer,Statement* statement){
     if (!strncmp(input_buffer->buffer,"insert",6)){
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-            input_buffer->buffer,"insert %d %s %s",&(statement->row_to_insert.id),
-             statement->row_to_insert.username, statement->row_to_insert.email
-        );
-        if(args_assigned<3){
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
+
+        //scanf when inputting more than it should buffer overflows so we'll first 
+        //break down the string into the three keywords and then check their length
+        //using strtok
+        // statement->type = STATEMENT_INSERT;
+        // int args_assigned = sscanf(
+        //     input_buffer->buffer,"insert %d %s %s",&(statement->row_to_insert.id),
+        //      statement->row_to_insert.username, statement->row_to_insert.email
+        // );
+        // if(args_assigned<3){
+        //     return PREPARE_SYNTAX_ERROR;
+        // }
+        // return PREPARE_SUCCESS;
     }
     if (!strcmp(input_buffer->buffer,"select")){
         statement->type = STATEMENT_SELECT;
@@ -237,11 +268,18 @@ int main(int argc, char* argv[]){
         //if not a meta command ==> SQL command
         Statement statement;
         switch(prepare_statement(input_buffer,&statement)){
-            case(PREPARE_SUCCESS): break;
+            case(PREPARE_SUCCESS): 
+                break;
             case(PREPARE_UNRECOGNIZED_STATEMENT):
                 printf("Unrecognized keyword at start of '%s'.\n",
                     input_buffer->buffer);
                 //Read input again
+                continue;
+            case(PREPARE_NEGATIVE_ID):
+                printf("ID must be positive. \n");
+                continue;
+            case(PREPARE_STRING_TOO_LONG):
+                printf("String is too long.\n");
                 continue;
         } 
 
--- spec/db.rb
+++ spec/db.rb
@@ -14,20 +14,19 @@ describe 'database' do
       raw_output.split("\n")
     end
   
-    it 'allows inserting strings that are the maximum length' do
-        long_username = "a"*32
-        long_email = "a"*255
-        script = [
-          "insert 1 #{long_username} #{long_email}",
-          "select",
-          ".exit",
-        ]
-        result = run_script(script)
-        expect(result).to match_array([
-          "db > Executed.",
-          "db > (1, #{long_username}, #{long_email})",
-          "Executed.",
-          "db > ",
-        ])
-      end
+    it 'prints error message if strings are too long' do
+      long_username = "a"*33
+      long_email = "a"*256
+      script = [
+        "insert 1 #{long_username} #{long_email}",
+        "select",
+        ".exit",
+      ]
+      result = run_script(script)
+      expect(result).to match_array([
+        "db > String is too long.",
+        "db > Executed.",
+        "db > ",
+      ])
+    end
   end
